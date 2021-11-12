2f62d77e8a938f3e5cee1e140f7851e1035ca249
Assadi <assadi.halifa@gmail.com>
2021-11-12 14:27:18 +0100

    Update index.js

--- client/src/components/navigation/Navbar/index.js
+++ client/src/components/navigation/Navbar/index.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import SearchBar from "./SearchBar";
 import styles from "./style.module.css";
+import AuthContext from "../../../context/AuthContext";
 
 const Navbar = () => {
   const url = process.env.REACT_APP_HOST;
+  const authContext = useContext(AuthContext).userAuth;
 
   return (
     <div className={styles.navContainer}>
       <SearchBar />
       <div className={styles.leftSide}>
-        <a href={`${url}/auth/twitch/connect`} className={styles.twitchConnect}>
-          Se connecter
-        </a>
+        {!authContext.id ? (
+          <a
+            href={`${url}/auth/twitch/connect`}
+            className={styles.twitchConnect}
+          >
+            Se connecter
+          </a>
+        ) : (
+          <a href={`${url}/auth/logout`} className={styles.twitchConnect}>
+            Deconnexion
+          </a>
+        )}
       </div>
     </div>
   );
