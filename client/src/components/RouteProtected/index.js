41951d91688223a869168a0d8fc081c1f72c8fc9
Assadi <assadi.halifa@gmail.com>
2021-11-12 03:58:42 +0100

    Update index.js

--- client/src/components/RouteProtected/index.js
+++ client/src/components/RouteProtected/index.js
@@ -2,14 +2,14 @@ import React, { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import AuthContext from "../../context/AuthContext";
 
-const RouteProtected = ({ children, logged }) => {
+const RouteProtected = ({ children, logged, path }) => {
   const location = useLocation();
   const authContext = useContext(AuthContext);
 
   //let logged = authContext;
 
   if (!logged) {
-    return <Navigate to="/" state={{ from: location }} replace />;
+    return <Navigate to="/" state={{ path }} replace />;
   }
 
   return children;
